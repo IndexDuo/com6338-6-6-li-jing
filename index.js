12bb94ae96b7d232dd56f01bb468c751fd82cb98
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-02-27 04:39:14 +0000

    Feb 26, 2025, 11:39 PM

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ var navList = hamburgerButton.nextElementSibling;
 // console.log(navList);
 
 addEventListener("resize", () => {
-    if (window.innerWidth < 800) {
-        // console.log("small screen" + window.innerWidth);
-        hamburgerButton.addEventListener("click", () => {
-            navList.classList.add("show-menu");
-        });
-    } else {
-        // console.log("large screen" + window.innerWidth);
-    }
+    screenSmall = window.innerWidth < 800 ? true : false;   
 });
+
+
+if(screenSmall){
+    // console.log("small screen" + window.innerWidth);
+    hamburgerButton.addEventListener("click", () => {
+        navList.classList.add("show-menu");
+    });
+} else {
+    // console.log("large screen" + window.innerWidth);
+}
